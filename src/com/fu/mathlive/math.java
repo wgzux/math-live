8579b5972987138d971b228f09da80c8af898375
wgzux <pqvu.92nvt@gmail.com>
2024-12-14 15:17:44 +0700

    LAN DAU DUA CODE LEN SEVER

---
+++ src/com/fu/mathlive/math.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fu.mathlive;
+
+/**
+ *
+ * @author Nup
+ */
+public class math {
+    public static void main(String[] args) {
+        int x=5;
+        System.out.println("HELLO WORLD" + x);     
+
+    }
+}
